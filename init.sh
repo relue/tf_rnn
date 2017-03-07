3db6d96b2914acd1b65cbd94fa890e7c0b32f7d2
monty <you@example.com>
2017-03-07 11:50:43 +0100

    fastupload

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ export CUDA_HOME="/sw/taurus/libraries/cuda/8.0.44"
 #env/bin/python2.7 tensorflow/tensorflow/examples/tutorials/mnist/fully_connected_feed.py 
 #srun  --gres=gpu:4 --time=00:30:00 --mem=40110  --pty ~/pythonProjects/env/bin/python2.7 $1
 
-srun --cpus-per-task=16 --time=00:30:00 --mem=40110  --pty ~/pythonProjects/env/bin/python2.7 $1
+srun --cpus-per-task=8 --time=00:30:00 --mem=30110  --pty ~/pythonProjects/env/bin/python2.7 $1
 
 
 
