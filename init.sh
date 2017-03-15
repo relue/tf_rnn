9fb139b7adc6e180b1170fa4e9d7c9262118b9a8
Simon Pickert <pickert.simon@gmail.com>
2017-03-15 12:20:18 +0100

    fastupload

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#module load gcc/4.9.1
+module load gcc/4.9.1
 module load cuda/8.0.44
 
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/sw/taurus/libraries/cuda/8.0.44/lib64:/sw/taurus/libraries/cuda/8.0.44/extras/CUPTI/lib64:${HOME}/libraries/cudnn/lib64:"
