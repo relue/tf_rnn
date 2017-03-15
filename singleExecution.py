53beb6a67c3232a5036fd3fac31d0195fd1e0026
Simon Pickert <pickert.simon@gmail.com>
2017-03-15 17:25:46 +0100

    fastupload

--- singleExecution.py
+++ singleExecution.py
@@ -2,7 +2,7 @@ import modelKeras
 import json
 import subprocess
 
-pre = subprocess.Popen("~/pythonProjects/tf_rnn/preInit.sh ", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+pre = subprocess.Popen("sh ~/pythonProjects/tf_rnn/preInit.sh ", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 data=json.loads(argv[1])
 modelOut = modelKeras.kerasModel(**data)
