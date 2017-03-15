4cf80756b0282f23dbf5d830fecb8b845808c7be
Simon Pickert <pickert.simon@gmail.com>
2017-03-15 17:21:09 +0100

    fastupload

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 git add . && git commit -m "fastupload"  && git push
-ssh -tX s2071275@login2.zih.tu-dresden.de 'ssh -tX s2071275@taurus.hrsk.tu-dresden.de "source ~/.bashrc;source ~/.profile;cd pythonProjects/tf_rnn;git pull;
+ssh -tX s2071275@login2.zih.tu-dresden.de 'ssh -tX s2071275@taurus.hrsk.tu-dresden.de "source ~/.bashrc;source ~/.profile;
+cd pythonProjects/tf_rnn;git pull; sh preInit.sh;
 ~/pythonProjects/env/bin/python2.7 ~/pythonProjects/tf_rnn/parallelExecutor.py"'
