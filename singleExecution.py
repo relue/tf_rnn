33292dfe19c6038248b0b2a165d37c36283941d3
Simon Pickert <pickert.simon@gmail.com>
2017-03-15 17:39:10 +0100

    fastupload

--- singleExecution.py
+++ singleExecution.py
@@ -1,7 +1,8 @@
 import modelKeras
 import json
 import subprocess
-
+import logging
+logging.basicConfig(filename='parallelExecDetail.log',level=logging.DEBUG)
 pre = subprocess.Popen("sh ~/pythonProjects/tf_rnn/preInit.sh ", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 data=json.loads(argv[1])
