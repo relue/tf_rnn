cdcf71a1da09c550192a47f1c880dd54be95ff53
Simon Pickert <pickert.simon@gmail.com>
2017-03-20 15:30:29 +0100

    fastupload

--- parallelExecutor.py
+++ parallelExecutor.py
@@ -39,14 +39,14 @@ for el in permMatrix:
         "indexID" : permIndex
     }
     data_str=json.dumps(setting)
-
+    print data_str
 #env/bin/python2.7 tensorflow/tensorflow/examples/tutorials/mnist/fully_connected_feed.py
-    command = preCommand+"srun --cpus-per-task=1 --time=00:30:00 --mem=1110 ~/pythonProjects/env/bin/python2.7 -W ignore ~/pythonProjects/tf_rnn/singleExecution.py '"+data_str + "' "
+    command = preCommand+"srun --cpus-per-task=1 --time=00:30:00 --mem=3110 ~/pythonProjects/env/bin/python2.7 -W ignore ~/pythonProjects/tf_rnn/singleExecution.py '"+data_str + "' "
     p = subprocess.Popen(command,  stdout=log, stderr=log, shell=True)
     time.sleep(3)
     if permIndex >= maxIters:
         break
-    permIndex += 1500
+    permIndex += 1
     print 'permIndex:'+str(permIndex)
 
 
