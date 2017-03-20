aa21620d0bd3ce963cd5533b6fb208b5667802a7
Simon Pickert <pickert.simon@gmail.com>
2017-03-20 15:01:54 +0100

    fastupload

--- collectResults.py
+++ collectResults.py
@@ -1,16 +1,37 @@
 import numpy as np
 import pandas as pd
 import os
+import sys
 
 dirName= "jobResults/"
 dirList = os.listdir(dirName)
 dirList.sort()
 first = True
 dfList = []
-
+errors = []
+running = []
+results = []
+maxAwaiting = sys.argv[1]
 for s in dirList:
+    name, id = s.split("_")
+    if name == "error":
+        size = os.stat(dirName+s).st_size
+        print s+' has size: '+str(size)
+        if size > 3600:
+            errors.append(id)
+    if name == "output":
+        running.append(id)
     if "result" in s:
        dfList.append(pd.read_pickle(dirName+s))
+       results.append(id)
 
 result = pd.concat(dfList)
+result = result.sort_values(['val_loss'], ascending=[True])
+
+print str(len(errors))+' Fehler in:'
+print errors
+
+print str(len(running))+' laufende Jobs'
+print str(len(results))+' abgeschlossene Jobs'
+print maxAwaiting+ 'jobs geplant'
 print result
--- singleExecution.py
+++ singleExecution.py
@@ -2,22 +2,25 @@
 import sys
 import json
 data=json.loads(sys.argv[1])
-sys.stdout = open('jobResults/output'+str(data["indexID"]), 'w')
-sys.stderr = open('jobResults/error'+str(data["indexID"]), 'w')
-import modelKeras
+sys.stdout = open('jobResults/output_'+str(data["indexID"]), 'w')
+sys.stderr = open('jobResults/error_'+str(data["indexID"]), 'w')
 
-import time
-import logging
-import numpy as np
-import pandas as pd
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    import modelKeras
+    import time
+    import logging
+    import numpy as np
+    import pandas as pd
 
-start_time = time.time()
-modelOut = modelKeras.kerasModel(**data)
-data['loss'] = modelOut.history['loss'][-1]
-data['val_loss'] = modelOut.history['val_loss'][-1]
-data['exec_time'] = (time.time() - start_time)
-columns = data.keys() + ['loss', 'val_loss','exec_time']
+    start_time = time.time()
+    modelOut = modelKeras.kerasModel(**data)
+    data['loss'] = modelOut.history['loss'][-1]
+    data['val_loss'] = modelOut.history['val_loss'][-1]
+    data['exec_time'] = (time.time() - start_time)
+    columns = data.keys() + ['loss', 'val_loss','exec_time']
 
-singleResult = pd.DataFrame(data, index=[data["indexID"]])
-singleResult.to_pickle("jobResults/result"+str(data["indexID"]))
-print singleResult
+    singleResult = pd.DataFrame(data, index=[data["indexID"]])
+    singleResult.to_pickle("jobResults/result_"+str(data["indexID"]))
+    print singleResult
