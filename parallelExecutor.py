855815ced8f4cb29117636355b4d5c16e1f646a7
Simon Pickert <pickert.simon@gmail.com>
2017-03-20 12:11:55 +0100

    fastupload

--- parallelExecutor.py
+++ parallelExecutor.py
@@ -43,7 +43,7 @@ for el in permMatrix:
     data_str=json.dumps(setting)
 
 #env/bin/python2.7 tensorflow/tensorflow/examples/tutorials/mnist/fully_connected_feed.py
-    command += preCommand+"srun --cpus-per-task=16 --time=00:30:00 --mem=30110 ~/pythonProjects/env/bin/python2.7 -W ignore ~/pythonProjects/tf_rnn/singleExecution.py '"+data_str + "';"
+    command += preCommand+"srun --cpus-per-task=16 --time=00:30:00 --mem=30110 ~/pythonProjects/env/bin/python2.7 -W ignore ~/pythonProjects/tf_rnn/singleExecution.py '"+data_str + "' &"
     if permIndex % 100 == 0:
         p = subprocess.Popen(command,  stdout=log, stderr=log, shell=True)
         print command
