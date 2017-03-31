dffd81700d9a73df2ce53910fa345ca8a7aee66d
Simon Pickert <pickert.simon@gmail.com>
2017-03-31 18:25:30 +0200

    fastupload

--- experimentConfig.py
+++ experimentConfig.py
@@ -1,11 +1,29 @@
 import collections
 
+'''Umszusetzen:
+    - Dropout
+    - L1 Penalty
+    - Activation Function
+'''
 class Config():
+    parameterTypeDiscrete = {
+        "learningRate": False,
+        "DropoutProp": False,
+        "l1Penalty": False,
+        "activationFunction": True,
+        "hiddenNodes": True,
+        "optimizer": True,
+        "timeWindow": True,
+        "batchSize": True,
+        "hiddenNodes": True,
+        "hiddenLayers": True,
+        "weightInit": True,
+    }
     parametersAddtionalInput = collections.OrderedDict((
     ("learningRate", [0.001, 0.01, 0.05]),
     #("hiddenLayer"  , [1, 2, 3, 4]),
     ("hiddenNodes" , [32, 50, 62, 128,256]),
-    ("optimizers" , ['adam', 'sgd']),
+    ("optimizer" , ['adam']),
     ("timeWindow" , [12, 24, 36]),
     ("batchSize" , [1,10]),
     ("epochSize" , [30]),
@@ -19,9 +37,11 @@ class Config():
     ("learningRate", [0.001, 0.01, 0.05, 0.1, 0.2]),
     #("hiddenLayer"  , [1, 2, 3, 4]),
     ("hiddenNodes" , [2, 4, 8, 16, 32, 50, 62, 128,256]),
-    ("optimizers" , ['adam', 'sgd', 'rms','ada']),
+    ("optimizer" , ['adam', 'sgd', 'rms','ada']),
     ("timeWindow" , [1, 12, 24, 36, 48, 96, 144, 120, 168]),
     ("batchSize" , [1,10, 100]),
     ("epochSize" , [30]),
     #("activationFunction" , ["tanh", "sigmoid"])
     ))
+
+    fullParameters =
