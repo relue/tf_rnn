f46010fb938ac87bcf46d232e9b7ee3387981ba4
simon <pickert.simon@gmail.com>
2017-04-01 22:32:35 +0200

    fastupload

--- experimentConfig.py
+++ experimentConfig.py
@@ -24,17 +24,6 @@ class Config():
     #("activationFunction" , ["tanh", "sigmoid"])
     ))
 
-    standardParamater = collections.OrderedDict((
-    ("learningRate", [0.001, 0.01, 0.05, 0.1, 0.2]),
-    #("hiddenLayer"  , [1, 2, 3, 4]),
-    ("hiddenNodes" , [2, 4, 8, 16, 32, 50, 62, 128,256]),
-    ("optimizer" , ['adam', 'sgd', 'rms','ada']),
-    ("timeWindow" , [1, 12, 24, 36, 48, 96, 144, 120, 168]),
-    ("batchSize" , [1,10, 100]),
-    ("epochSize" , [30]),
-    #("activationFunction" , ["tanh", "sigmoid"])
-    ))
-
     parameterTypeDiscrete = {
         "learningRate": False,
         "DropoutProp": False,
@@ -45,6 +34,7 @@ class Config():
         "timeWindow": True,
         "batchSize": True,
         "hiddenLayers": True,
+        "earlyStopping": True,
         "weightInit": True
     }
 
@@ -55,13 +45,14 @@ class Config():
     }
 
     discreteParameterRanges = {
-        "activationFunction": ["hypT", "Sig", "Relu"],
+        "activationFunction": ["tanh", "sigmoid", "relu"],
         "hiddenNodes": range(10,1001),
         "optimizer": ['adam', 'sgd', 'rms','ada', 'adadelta'],
         "timeWindow": range(10,137),
         "batchSize": range(1,101),
         "hiddenLayers": range(1,11),
-        "weightInit": ["zero", "one", "normal", "glorot_uniform", "lecun_uniform", "glorot_normal"]
+        "weightInit": ["zero", "one", "normal", "glorot_uniform", "lecun_uniform", "glorot_normal"],
+        "earlyStopping": [True, False],
     }
 
     def generateRandomVektor(self, parameterList):
