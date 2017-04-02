f3249fc20db4eb297cb5f231a05b13b78875ee0c
simon <pickert.simon@gmail.com>
2017-04-02 12:46:01 +0200

    fastupload

--- parallelExecutor.py
+++ parallelExecutor.py
@@ -11,7 +11,7 @@ def createBatchFile(singleCommand,parameters, id):
         with open("batchScripts/script"+str(id)+".sh", "wt") as fout:
             for line in fin:
                 line = line.replace('?job?', singleCommand)
-                line = line.replace('?jobname?', parameters)
+                line = line.replace('?jobname?', "blubb")
                 fout.write(line)
 import experimentConfig
 
