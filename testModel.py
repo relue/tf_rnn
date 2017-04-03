a9d8e654657d3d29ae46759df295b569c0338e72
Simon Pickert <pickert.simon@gmail.com>
2017-04-03 16:18:56 +0200

    fastupload

---
+++ testModel.py
@@ -0,0 +1,18 @@
+import json
+import modelKeras
+
+modelInput = "{}"
+#modelInput = '{"optimizer": "adam", "hiddenNodes": 2, "timeWindow": 1, "batchSize": 1, "indexID": 2, "learningRate": 0.001, "epochSize": 10}'
+data=json.loads(modelInput)
+data["indexID"] = 1
+data["isShow"] = True
+data["createHTML"] = True
+data["cellType"] = "rnn"
+data["timeWindow"] = 33
+
+
+import numpy as np
+import pandas as pd
+
+
+modelOut = modelKeras.KerasModel(**data)
