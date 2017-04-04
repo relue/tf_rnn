e75d5e34b1f2bf7dddab861e0e24bec63045bcc9
Simon Pickert <pickert.simon@gmail.com>
2017-04-04 13:52:56 +0200

    fastupload

--- HyperoptStartWorkers.py
+++ HyperoptStartWorkers.py
@@ -28,6 +28,7 @@ for i in range(1, workerCount):
 
     p = subprocess.Popen("sbatch batchScripts/hyperOptScript.sh", stdout=log, stderr=log, shell=True)
 
+p = subprocess.Popen("source ../env/bin/activate; python HyperoptOptimizer.py ", stdout=log, stderr=log, shell=True)
 while 1:
     time.sleep(10)
     p= subprocess.Popen("cat hyperoptStartWorker.log", stdout=subprocess.PIPE, stderr=None, shell=True)
