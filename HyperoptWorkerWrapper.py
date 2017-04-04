a73375f7b0ecab419f0a4f1917ebe427519d3621
Simon Pickert <pickert.simon@gmail.com>
2017-04-04 11:55:48 +0200

    fastupload

--- HyperoptWorkerWrapper.py
+++ HyperoptWorkerWrapper.py
@@ -3,7 +3,7 @@ import time
 ip = '172.24.32.17'
 log = open("hyperoptWorker.log", "w")
 workerCommand= 'source ~/pythonProjects/tf_rnn/preInit.sh ; hyperopt-mongo-worker --mongo='+ip+':27017/foo_db --poll-interval=0.1'
-
+print workerCommand
 p = subprocess.Popen(workerCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 for line in p.stdout:
     print line
