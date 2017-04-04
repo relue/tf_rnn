2e0297e2720b6527a5871deeca45c2336627d35b
Simon Pickert <pickert.simon@gmail.com>
2017-04-04 11:23:42 +0200

    fastupload

--- HyperoptWorkerWrapper.py
+++ HyperoptWorkerWrapper.py
@@ -4,5 +4,5 @@ ip = '172.24.32.17'
 log = open("hyperoptWorker.log", "w")
 workerCommand= 'hyperopt-mongo-worker --mongo='+ip+':27017/foo_db --poll-interval=0.1'
 
-p = subprocess.Popen("sbatch batchScripts/hyperOptScript.sh", stdout=log, stderr=log, shell=True)
+p = subprocess.Popen(workerCommand, stdout=log, stderr=log, shell=True)
 
