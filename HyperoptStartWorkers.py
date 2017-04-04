e5adcae685d836b5b48f26d8d749e98c37411e0f
Simon Pickert <pickert.simon@gmail.com>
2017-04-04 13:48:49 +0200

    fastupload

--- HyperoptStartWorkers.py
+++ HyperoptStartWorkers.py
@@ -19,7 +19,7 @@ for filename in os.listdir("batchScripts/"):
     os.remove("batchScripts/"+filename)
 
 ip = '172.24.32.17'
-workerCount = 10000
+workerCount = 1000
 log = open("hyperoptStartWorker.log", "w")
 
 createBatchFile("srun --cpus-per-task=1 --time=01:00:00 --mem=3110 ~/pythonProjects/env/bin/python2.7 -W ignore ~/pythonProjects/tf_rnn/HyperoptWorkerWrapper.py")
