1d2034bbd41b55b100dbf81139eda2ee08d9cbc6
monty <you@example.com>
2017-04-05 19:39:46 +0200

    fastupload

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 git add . && git commit -m "fastupload"  && git push
 ssh -tX s2071275@login2.zih.tu-dresden.de 'ssh -tX s2071275@taurus.hrsk.tu-dresden.de "source ~/.bashrc;source ~/.profile;
-cd pythonProjects/tf_rnn;git stash;git pull; sh preInit.sh;
-~/pythonProjects/env/bin/python2.7 ~/pythonProjects/tf_rnn/parallelExecutor.py"'
+cd pythonProjects/tf_rnn;git stash;git pull; sh cpuExec.sh ~/pythonProjects/tf_rnn/parallelExecutor.py"'
