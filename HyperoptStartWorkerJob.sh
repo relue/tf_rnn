1433dec0b0b77ad58b4d64ef30725efbc581e665
monty <you@example.com>
2017-04-06 18:21:49 +0200

    fastupload

--- HyperoptStartWorkerJob.sh
+++ HyperoptStartWorkerJob.sh
@@ -3,4 +3,5 @@
 #SBATCH --ntasks=1
 #SBATCH --mem=10000
 #SBATCH --time=24:00:00
+#SBATCH --job-name=startJobs
 srun --time=24:00:00 ~/pythonProjects/env/bin/python2.7 -W ignore HyperoptStartWorkers.py
