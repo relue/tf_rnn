8bb2ec52c5b5d8515114c173aeb3aae24d0af4a7
monty <you@example.com>
2017-04-07 12:53:51 +0200

    fastupload

--- HyperoptStartWorkers.py
+++ HyperoptStartWorkers.py
@@ -25,10 +25,12 @@ s.close()
 workerCount = 10
 log = open("logs/hyperoptStartWorker.log", "w")
 logDB = open("logs/mongo.log", "w")
-createBatchFile("srun --time=02:00:00 ~/pythonProjects/env/bin/python2.7 -W ignore ~/pythonProjects/tf_rnn/HyperoptWorkerWrapper.py "+ip)
-p = subprocess.Popen("mongod --dbpath ~/mongo/mongodb/mongodb-linux-x86_64-3.4.2/data/db", stdout=logDB, stderr=logDB, shell=True)
-p = subprocess.Popen("source ../env/bin/activate; python HyperoptOptimizer.py "+ip, stdout=log, stderr=log, shell=True)
+createBatchFile("srun --time=02:00:00 source ~/pythonProjects/tf_rnn/preInit.sh; source ~/pythonProjects/env/bin/activate; hyperopt-mongo-worker --mongo="+ip+":27017/foo_db --poll-interval=0.1 ")
+startDB = "mongod --dbpath ~/mongo/mongodb/mongodb-linux-x86_64-3.4.2/data/db"
+startOptimizer = "source ../env/bin/activate; python HyperoptOptimizer.py "+ip
 
+p = subprocess.Popen(startDB, stdout=logDB, stderr=logDB, shell=True)
+p = subprocess.Popen(startOptimizer, stdout=log, stderr=log, shell=True)
 for i in range(1, workerCount):
     p = subprocess.Popen("sbatch hyperOptScriptExecute.sh", stdout=log, stderr=log, shell=True)
     time.sleep(0.3)
