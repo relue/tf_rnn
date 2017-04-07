861278e3da5884cdd7b25ac929da31f245eb0685
monty <you@example.com>
2017-04-07 10:50:56 +0200

    fastupload

--- HyperoptStartWorkers.py
+++ HyperoptStartWorkers.py
@@ -27,6 +27,7 @@ log = open("logs/hyperoptStartWorker.log", "w")
 logDB = open("logs/mongo.log", "w")
 createBatchFile("srun --time=02:00:00 ~/pythonProjects/env/bin/python2.7 -W ignore ~/pythonProjects/tf_rnn/HyperoptWorkerWrapper.py "+ip)
 p = subprocess.Popen("mongod --dbpath ~/mongo/mongodb/mongodb-linux-x86_64-3.4.2/data/db", stdout=logDB, stderr=logDB, shell=True)
+p = subprocess.Popen("source ../env/bin/activate; python HyperoptOptimizer.py "+ip, stdout=log, stderr=log, shell=True)
 for i in range(1, workerCount):
     p = subprocess.Popen("sbatch batchScripts/hyperOptScript.sh", stdout=log, stderr=log, shell=True)
     time.sleep(0.3)
