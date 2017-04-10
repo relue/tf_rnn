83c4cf9875abc26441fe2f41385871f1937ec5b5
monty <you@example.com>
2017-04-10 17:44:09 +0200

    fastupload

--- HyperoptWorkerWrapper.sh
+++ HyperoptWorkerWrapper.sh
@@ -1,1 +1,1 @@
-source ~/pythonProjects/tf_rnn/preInit.sh ; source ~/pythonProjects/env/bin/activate; hyperopt-mongo-worker --mongo=$1:27017/foo_db --poll-interval=0.1
+source ~/pythonProjects/tf_rnn/preInit.sh ; source ~/pythonProjects/env/bin/activate; hyperopt-mongo-worker --reserve-timeout=10 --mongo=$1:27017/foo_db --poll-interval=10 --loop
