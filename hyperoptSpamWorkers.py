6cb92eee521b706f26e735fcdee22471dc538973
Simon Pickert <pickert.simon@gmail.com>
2017-04-11 15:43:18 +0200

    fastupload

--- hyperoptSpamWorkers.py
+++ hyperoptSpamWorkers.py
@@ -11,13 +11,22 @@ def createBatchFile(singleCommand):
                 line = line.replace('?job?', singleCommand)
                 fout.write(line)
 
+def createBatchFileGPU(singleCommand):
+    with open("hyperOptScript.sh", "rt") as fin:
+        with open("hyperOptScriptExecuteGPU.sh", "wt") as fout:
+            for line in fin:
+                line = line.replace('?job?', singleCommand)
+                fout.write(line)
+
 workerCount = 1000
 ip=sys.argv[1]
 
-createBatchFile("srun --gres=gpu:1 --time=02:00:00 --mem=6000 sh ~/pythonProjects/tf_rnn/HyperoptWorkerWrapper.sh "+ip)
+createBatchFile("srun --time=02:00:00 --mem=6000 sh ~/pythonProjects/tf_rnn/HyperoptWorkerWrapper.sh "+ip)
+createBatchFileGPU("srun --gres=gpu:1 --time=02:00:00 --mem=6000 sh ~/pythonProjects/tf_rnn/HyperoptWorkerWrapper.sh "+ip)
 
 for i in range(1, workerCount):
     p = subprocess.Popen("sbatch hyperOptScriptExecute.sh", stdout=log, stderr=log, shell=True)
+    p = subprocess.Popen("sbatch hyperOptScriptExecuteGPU.sh", stdout=log, stderr=log, shell=True)
     time.sleep(0.5)
 
 while 1:
