610889a14da7818888e20ebfee47aa61d1bf992b
Simon Pickert <pickert.simon@gmail.com>
2017-04-11 15:37:00 +0200

    fastupload

--- hyperOptScript.sh
+++ hyperOptScript.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #SBATCH --output=logs/slurmStartWorker.log
-#SBATCH --ntasks=1
-#SBATCH --mem=10000
 ?job?
--- hyperoptSpamWorkers.py
+++ hyperoptSpamWorkers.py
@@ -14,7 +14,7 @@ def createBatchFile(singleCommand):
 workerCount = 1000
 ip=sys.argv[1]
 
-createBatchFile("srun --time=02:00:00 --mem=2000 sh ~/pythonProjects/tf_rnn/HyperoptWorkerWrapper.sh "+ip)
+createBatchFile("srun --gres=gpu:1 --time=02:00:00 --mem=6000 sh ~/pythonProjects/tf_rnn/HyperoptWorkerWrapper.sh "+ip)
 
 for i in range(1, workerCount):
     p = subprocess.Popen("sbatch hyperOptScriptExecute.sh", stdout=log, stderr=log, shell=True)
