4249190e27f29d4d6472e1681bed992244667b28
Simon Pickert <pickert.simon@gmail.com>
2017-04-13 23:37:51 +0200

    fastupload

--- getDataframeFromHyperopt.py
+++ getDataframeFromHyperopt.py
@@ -5,11 +5,12 @@ import socket
 import numpy as np
 import pandas as pd
 import sys
+import dataExplore2
 with open('logs/ip.log', 'r') as f:
     first_line = f.readline()
 ip= first_line
 #finalFun
-trials = MongoTrials('mongo://'+ip+':27017/foo_db/jobs', exp_key='finalBeta')
+trials = MongoTrials('mongo://'+ip+':27017/foo_db/jobs', exp_key='finalBeta1')
 finished = []
 for row in trials.results:
     resultRow = row.to_dict()
@@ -24,8 +25,10 @@ for m in measures:
     columns.remove(m)
 newAlign = measures+columns
 dfF = dfF[newAlign]
-dfF = dfF.sort_values(['test_loss'], ascending=[True])
+dfF = dfF.sort_values(['val_rmse'], ascending=[True])
 print str(len(finished))+' results collected'
 print dfF
 dfF.to_pickle("randomSearchResults.pd")
+if sys.argv[1] == 1:
+    dataExplore2.showDF(dfF, False)
 
