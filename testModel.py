f2c12a7fa90a94dc26c2c05a7f279364e03b79c3
Simon Pickert <pickert.simon@gmail.com>
2017-04-14 00:54:19 +0200

    fastupload

--- testModel.py
+++ testModel.py
@@ -9,7 +9,7 @@ data["isShow"] = False
 data["createHTML"] = False
 data["earlyStopping"] = True
 data["epochSize"] = 8
-data["standardizationType"] = "zscore"
+data["standardizationType"] = "minmax"
 
 import numpy as np
 import pandas as pd
