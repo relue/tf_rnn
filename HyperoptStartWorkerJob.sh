abbd0aae07fc8af989cefdd3e5632f3cc498e184
Simon Pickert <pickert.simon@gmail.com>
2017-04-13 10:42:21 +0200

    fastupload

--- HyperoptStartWorkerJob.sh
+++ HyperoptStartWorkerJob.sh
@@ -2,6 +2,6 @@
 #SBATCH --output=logs/SlurmHyperoptStartWorkers.log
 #SBATCH --ntasks=1
 #SBATCH --mem=15000
-#SBATCH --time=24:00:00
+#SBATCH --time=12:00:00
 #SBATCH --job-name=startJobs
-srun --time=02:00:00 --mem=15000 ~/pythonProjects/env/bin/python2.7 -W ignore HyperoptStartWorkers.py
+srun --time=12:00:00 --mem=15000 ~/pythonProjects/env/bin/python2.7 -W ignore HyperoptStartWorkers.py
--- HyperoptStartWorkers.py
+++ HyperoptStartWorkers.py
@@ -37,6 +37,8 @@ while 1:
     if exec_time > 1800:
         os.killpg(os.getpgid(pMongo.pid), signal.SIGTERM)
         os.killpg(os.getpgid(pOpti.pid), signal.SIGTERM)
+        log.write(str(time.time())+'kill db process\n')  # python will convert \n to os.linesep
+
         time.sleep(30)
         pMongo = subprocess.Popen(startDB, stdout=logDB, stderr=logDB, shell=True, preexec_fn=os.setsid)
         pOpti = subprocess.Popen(startOptimizer, stdout=log, stderr=log, shell=True, preexec_fn=os.setsid)
