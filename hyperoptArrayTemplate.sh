f5b9c704906d16b2939d02fcfe0c78d551a33e5b
Simon Pickert <pickert.simon@gmail.com>
2017-04-18 15:08:12 +0200

    fastupload

--- hyperoptArrayTemplate.sh
+++ hyperoptArrayTemplate.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH -J HyWorkers
-#SBATCH --array 0-9
+#SBATCH --array 0-500
 #SBATCH -o logs/arr/arraytest-%A_%a.out
 #SBATCH -e logs/arr/arraytest-%A_%a.err
 #SBATCH --ntasks=1
 #SBATCH --mail-type=end
-#SBATCH --mail-user=simon.pickert@mailbox.tu-dresden.de
+#SBATCH --mail-user=1simon.pickert@mailbox.tu-dresden.de
 #SBATCH --time=01:00:00
 #SBATCH --ntasks=1
 #SBATCH --output=logs/slurmStartWorkerArray.log
-#SBATCH --mem-per-cpu=1500
+#SBATCH --mem-per-cpu=2500
 sh HyperoptWorkerWrapper.sh $1
