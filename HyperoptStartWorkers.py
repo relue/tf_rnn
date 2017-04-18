c79bf3e4f4d410e8b85b8b6df3744114091ec7c1
Simon Pickert <pickert.simon@gmail.com>
2017-04-18 19:40:49 +0200

    fastupload

--- HyperoptStartWorkers.py
+++ HyperoptStartWorkers.py
@@ -32,8 +32,8 @@ createBatchFile("srun --time=12:00:00 --mem-per-cpu=10000 ~/pythonProjects/env/b
 
 pMongo = subprocess.Popen(startDB, stdout=logDB, stderr=logDB, shell=True, preexec_fn=os.setsid)
 pOpti = subprocess.Popen(startOptimizer, stdout=log, stderr=log, shell=True, preexec_fn=os.setsid)
-p = subprocess.Popen("cd logs/cache/ && sbatch hyperoptArrayTemplate.sh "+ip, stdout=log, stderr=log, shell=True)
-p = subprocess.Popen("cd logs/cache/ && sbatch hyperoptArrayTemplateGPU.sh "+ip, stdout=log, stderr=log, shell=True)
+p = subprocess.Popen("cp hyperoptArrayTemplate.sh logs/cache/hyperoptArrayTemplate.sh && cd logs/cache/ && sbatch hyperoptArrayTemplate.sh "+ip, stdout=log, stderr=log, shell=True)
+p = subprocess.Popen("cp hyperoptArrayTemplateGPU.sh logs/cache/hyperoptArrayTemplateGPU.sh && cd logs/cache/ && sbatch hyperoptArrayTemplateGPU.sh "+ip, stdout=log, stderr=log, shell=True)
 
 start_time = time.time()
 while 1:
