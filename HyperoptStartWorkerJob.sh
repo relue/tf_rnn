ed628648f4cc7c053e7fcafb3403954c334a7c93
Simon Pickert <pickert.simon@gmail.com>
2017-04-18 14:59:44 +0200

    fastupload

--- HyperoptStartWorkerJob.sh
+++ HyperoptStartWorkerJob.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --output=logs/SlurmHyperoptStartWorkers.log
 #SBATCH --time=24:00:00
-#SBATCH --mem-per-cpu=20000
+#SBATCH --mem-per-cpu=10000
 #SBATCH --job-name=startJobs
 #SBATCH --ntasks=1
 
-srun --mem-per-cpu=20000 --time=24:00:00 --ntasks=1  ~/pythonProjects/env/bin/python2.7 -W ignore HyperoptStartWorkers.py
+srun --mem-per-cpu=20000 --time=12:00:00 --ntasks=1  ~/pythonProjects/env/bin/python2.7 -W ignore HyperoptStartWorkers.py
