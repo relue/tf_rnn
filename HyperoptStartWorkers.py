2fdd70fe2a454229c496e361fe2ce69330cdff05
Simon Pickert <pickert.simon@gmail.com>
2017-04-22 17:42:20 +0200

    fastupload

--- HyperoptStartWorkers.py
+++ HyperoptStartWorkers.py
@@ -24,7 +24,7 @@ logDB = open("logs/mongo.log", "w")
 ipLog = open("logs/ip.log", "w")
 ipLog.write(ip)
 ipLog.close()
-startDB = "ulimit -u 100000 && mongod --dbpath ~/mongo/mongodb/mongodb-linux-x86_64-3.4.2/data/db"
+startDB = "ulimit -u 100000 && mongod --dbpath /lustre/ssd/s2071275/mongo/db"
 startOptimizer = "ulimit -u 100000 && source ../env/bin/activate; python HyperoptOptimizer.py "+ip
 #foo = Popen("source the_script.sh", shell=True, executable="/bin/bash")
 #createBatchFile("srun --time=12:00:00 --mem-per-cpu=10000 ~/pythonProjects/env/bin/python2.7 -W ignore ~/pythonProjects/tf_rnn/hyperoptSpamWorkers.py "+ip)
