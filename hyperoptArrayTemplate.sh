e8b8c33f66417535a1ac535a28d602eaa31d913d
Simon Pickert <pickert.simon@gmail.com>
2017-05-03 08:22:42 +0200

    fastupload

--- hyperoptArrayTemplate.sh
+++ hyperoptArrayTemplate.sh
@@ -4,6 +4,6 @@
 #SBATCH -o logs/arr/cpu-%a.out
 #SBATCH -e logs/arr/cpu-%a.err
 #SBATCH --ntasks=1
-#SBATCH --time=23:00:00
+#SBATCH --time=20:00:00
 #SBATCH --mem-per-cpu=4500
 sh HyperoptWorkerWrapper.sh $1
