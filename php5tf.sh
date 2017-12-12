1ae72dfc4ac75a9f6c556b9a41618741e2c85f82
Simon Pickert <pickert.simon@gmail.com>
2017-12-12 11:27:28 +0100

    fastupload

--- php5tf.sh
+++ php5tf.sh
@@ -9,4 +9,4 @@ module load h5py/2.6.0-intel-2016.03-GCC-5.3-Python-3.5.2-HDF5-1.8.17-serial
 
 export KERAS_BACKEND=tensorflow      # configure Keras to use tensorflow
 
-srun python singleExecution.py
+srun python singleExecution.py "{\"indexID\": 2}" 1
